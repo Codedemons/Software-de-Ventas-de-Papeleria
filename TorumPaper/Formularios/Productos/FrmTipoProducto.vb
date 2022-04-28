d5bcca5495620c896c5de63eaf1c9401f7785f83
Code <codededemonscm@gmail.com>
2022-04-27 23:57:53 -0500

    Modificaciones de las ventanas

--- TorumPaper/Formularios/Productos/FrmTipoProducto.vb
+++ TorumPaper/Formularios/Productos/FrmTipoProducto.vb
@@ -7,9 +7,16 @@
 ' To change this template use Tools | Options | Coding | Edit Standard Headers.
 '
 Public Partial Class FrmTipoProducto
+	Private ctrl As ClsTorumPaper	
+	
+	Private t As New Data.DataTable
+	Private v As New Data.DataView
+	
 	Public Sub New()
 		' The Me.InitializeComponent call is required for Windows Forms designer support.
 		Me.InitializeComponent()
+		
+		Me.ctrl = New ClsTorumPaper
 		
 		'
 		' TODO : Add constructor code after InitializeComponents
@@ -17,6 +24,47 @@ Public Partial Class FrmTipoProducto
 	End Sub
 	
 	Sub TxtClaveMarcaTextChanged(sender As Object, e As EventArgs)
+		
+	End Sub
+	
+	
+	Sub BtnRegistrarClick(sender As Object, e As EventArgs)
+		
+		'Guarda la sentencia en un string 
+		Dim insertarRegistro As String = "INSERT INTO tipoproducto (claveTipoProducto, descripcionTipoProducto) VALUES ('" & Me.txtClaveTipo.Text & "', '" & Me.txtDescripcion.text & "');"
+		
+		'envia y valida si el registro se realizo
+		If Me.ctrl.insertarRegistro(insertarRegistro) Then
+				MsgBox("Se realizó el registro")
+			End If
+	End Sub
+	
+	Sub FrmTipoProductoLoad(sender As Object, e As EventArgs)
+		
+		Dim consultaString As String = "select * from tipoproducto;"
+		Dim ctrl As New ClsTorumPaper
+		Me.t=ctrl.traerDatos(consultaString)
+		Me.v=New Data.DataView(t)
+		Me.dGVTipProductos.DataSource =Me.v.ToTable
+		
+	End Sub
+	
+	
+	Sub BtnActualizarClick(sender As Object, e As EventArgs)
+		
+		Dim consultaString As String = "select * from tipoproducto;"
+		Dim ctrl As New ClsTorumPaper
+		Me.t=ctrl.traerDatos(consultaString)
+		Me.v=New Data.DataView(t)
+		Me.dGVTipProductos.DataSource =Me.v.ToTable	
+		
+	End Sub
+	
+	Sub TextBox1TextChanged(sender As Object, e As EventArgs)
+		
+		Me.v=New Data.DataView(t)
+		Me.v.RowFilter="claveTipoProducto like '%" & Me.textBclave.Text & "%'"
+		Me.dGVTipProductos.DataSource = Me.v.ToTable	
 		
 	End Sub
 End Class
--- TorumPaper/Formularios/Proveedores/FrmProveedores.Designer.vb
+++ TorumPaper/Formularios/Proveedores/FrmProveedores.Designer.vb
@@ -34,6 +34,7 @@ Partial Class FrmProveedores
 	''' </summary>
 	Private Sub InitializeComponent()
 		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmProveedores))
+		Dim dataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
 		Me.lblTitulo = New System.Windows.Forms.Label()
 		Me.lblCalificacion = New System.Windows.Forms.Label()
 		Me.nUDCalificacion = New System.Windows.Forms.NumericUpDown()
@@ -45,12 +46,12 @@ Partial Class FrmProveedores
 		Me.lbltBuscar = New System.Windows.Forms.Label()
 		Me.btnActualizar = New System.Windows.Forms.Button()
 		Me.btnEliminar = New System.Windows.Forms.Button()
-		Me.dGVProveedores = New System.Windows.Forms.DataGridView()
 		Me.btnLimpiar = New System.Windows.Forms.Button()
 		Me.btnRegistrar = New System.Windows.Forms.Button()
-		Me.txtBRFC = New System.Windows.Forms.TextBox()
+		Me.txtBuscarRFC = New System.Windows.Forms.TextBox()
 		Me.cmbCalifAsc = New System.Windows.Forms.ComboBox()
-		Me.txtNombreProv = New System.Windows.Forms.TextBox()
+		Me.txtBuscarNombreProv = New System.Windows.Forms.TextBox()
+		Me.dGVProveedores = New System.Windows.Forms.DataGridView()
 		CType(Me.nUDCalificacion,System.ComponentModel.ISupportInitialize).BeginInit
 		CType(Me.dGVProveedores,System.ComponentModel.ISupportInitialize).BeginInit
 		Me.SuspendLayout
@@ -80,18 +81,18 @@ Partial Class FrmProveedores
 		'nUDCalificacion
 		'
 		Me.nUDCalificacion.BackColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(104,Byte),Integer), CType(CType(130,Byte),Integer))
-		Me.nUDCalificacion.Font = New System.Drawing.Font("Agency FB", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic),System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.nUDCalificacion.Font = New System.Drawing.Font("Agency FB", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic),System.Drawing.FontStyle))
 		Me.nUDCalificacion.ForeColor = System.Drawing.Color.White
-		Me.nUDCalificacion.Location = New System.Drawing.Point(138, 153)
+		Me.nUDCalificacion.Location = New System.Drawing.Point(127, 153)
 		Me.nUDCalificacion.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
 		Me.nUDCalificacion.Name = "nUDCalificacion"
-		Me.nUDCalificacion.Size = New System.Drawing.Size(184, 30)
+		Me.nUDCalificacion.Size = New System.Drawing.Size(195, 33)
 		Me.nUDCalificacion.TabIndex = 94
 		'
 		'txtTelefono
 		'
 		Me.txtTelefono.BackColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(104,Byte),Integer), CType(CType(130,Byte),Integer))
-		Me.txtTelefono.Font = New System.Drawing.Font("Agency FB", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.txtTelefono.Font = New System.Drawing.Font("Agency FB", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic),System.Drawing.FontStyle))
 		Me.txtTelefono.ForeColor = System.Drawing.Color.LightGray
 		Me.txtTelefono.Location = New System.Drawing.Point(339, 112)
 		Me.txtTelefono.Name = "txtTelefono"
@@ -102,7 +103,7 @@ Partial Class FrmProveedores
 		'txtCorreo
 		'
 		Me.txtCorreo.BackColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(104,Byte),Integer), CType(CType(130,Byte),Integer))
-		Me.txtCorreo.Font = New System.Drawing.Font("Agency FB", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.txtCorreo.Font = New System.Drawing.Font("Agency FB", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic),System.Drawing.FontStyle))
 		Me.txtCorreo.ForeColor = System.Drawing.Color.LightGray
 		Me.txtCorreo.Location = New System.Drawing.Point(21, 112)
 		Me.txtCorreo.Name = "txtCorreo"
@@ -113,7 +114,7 @@ Partial Class FrmProveedores
 		'txtNombre
 		'
 		Me.txtNombre.BackColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(104,Byte),Integer), CType(CType(130,Byte),Integer))
-		Me.txtNombre.Font = New System.Drawing.Font("Agency FB", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.txtNombre.Font = New System.Drawing.Font("Agency FB", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic),System.Drawing.FontStyle))
 		Me.txtNombre.ForeColor = System.Drawing.Color.LightGray
 		Me.txtNombre.Location = New System.Drawing.Point(339, 73)
 		Me.txtNombre.Name = "txtNombre"
@@ -124,7 +125,7 @@ Partial Class FrmProveedores
 		'txtRfc
 		'
 		Me.txtRfc.BackColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(104,Byte),Integer), CType(CType(130,Byte),Integer))
-		Me.txtRfc.Font = New System.Drawing.Font("Agency FB", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.txtRfc.Font = New System.Drawing.Font("Agency FB", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic),System.Drawing.FontStyle))
 		Me.txtRfc.ForeColor = System.Drawing.Color.LightGray
 		Me.txtRfc.Location = New System.Drawing.Point(21, 73)
 		Me.txtRfc.Name = "txtRfc"
@@ -139,11 +140,11 @@ Partial Class FrmProveedores
 		Me.btnBuscar.ForeColor = System.Drawing.Color.White
 		Me.btnBuscar.Image = CType(resources.GetObject("btnBuscar.Image"),System.Drawing.Image)
 		Me.btnBuscar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-		Me.btnBuscar.Location = New System.Drawing.Point(671, 63)
+		Me.btnBuscar.Location = New System.Drawing.Point(662, 248)
 		Me.btnBuscar.Name = "btnBuscar"
-		Me.btnBuscar.Size = New System.Drawing.Size(172, 47)
+		Me.btnBuscar.Size = New System.Drawing.Size(191, 47)
 		Me.btnBuscar.TabIndex = 110
-		Me.btnBuscar.Text = "  Buscar"
+		Me.btnBuscar.Text = "Editar"
 		Me.btnBuscar.UseVisualStyleBackColor = false
 		'
 		'lbltBuscar
@@ -164,12 +165,13 @@ Partial Class FrmProveedores
 		Me.btnActualizar.ForeColor = System.Drawing.Color.White
 		Me.btnActualizar.Image = CType(resources.GetObject("btnActualizar.Image"),System.Drawing.Image)
 		Me.btnActualizar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-		Me.btnActualizar.Location = New System.Drawing.Point(671, 381)
+		Me.btnActualizar.Location = New System.Drawing.Point(662, 381)
 		Me.btnActualizar.Name = "btnActualizar"
 		Me.btnActualizar.Size = New System.Drawing.Size(191, 47)
 		Me.btnActualizar.TabIndex = 108
 		Me.btnActualizar.Text = "   Actualizar"
 		Me.btnActualizar.UseVisualStyleBackColor = false
+		AddHandler Me.btnActualizar.Click, AddressOf Me.BtnActualizarClick
 		'
 		'btnEliminar
 		'
@@ -178,21 +180,12 @@ Partial Class FrmProveedores
 		Me.btnEliminar.ForeColor = System.Drawing.Color.White
 		Me.btnEliminar.Image = CType(resources.GetObject("btnEliminar.Image"),System.Drawing.Image)
 		Me.btnEliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-		Me.btnEliminar.Location = New System.Drawing.Point(671, 311)
+		Me.btnEliminar.Location = New System.Drawing.Point(662, 312)
 		Me.btnEliminar.Name = "btnEliminar"
 		Me.btnEliminar.Size = New System.Drawing.Size(191, 51)
 		Me.btnEliminar.TabIndex = 107
 		Me.btnEliminar.Text = "Eliminar"
 		Me.btnEliminar.UseVisualStyleBackColor = false
-		'
-		'dGVProveedores
-		'
-		Me.dGVProveedores.BackgroundColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(104,Byte),Integer), CType(CType(130,Byte),Integer))
-		Me.dGVProveedores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-		Me.dGVProveedores.Location = New System.Drawing.Point(25, 248)
-		Me.dGVProveedores.Name = "dGVProveedores"
-		Me.dGVProveedores.Size = New System.Drawing.Size(615, 180)
-		Me.dGVProveedores.TabIndex = 106
 		'
 		'btnLimpiar
 		'
@@ -201,9 +194,9 @@ Partial Class FrmProveedores
 		Me.btnLimpiar.ForeColor = System.Drawing.Color.White
 		Me.btnLimpiar.Image = CType(resources.GetObject("btnLimpiar.Image"),System.Drawing.Image)
 		Me.btnLimpiar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-		Me.btnLimpiar.Location = New System.Drawing.Point(671, 128)
+		Me.btnLimpiar.Location = New System.Drawing.Point(662, 128)
 		Me.btnLimpiar.Name = "btnLimpiar"
-		Me.btnLimpiar.Size = New System.Drawing.Size(172, 55)
+		Me.btnLimpiar.Size = New System.Drawing.Size(191, 55)
 		Me.btnLimpiar.TabIndex = 105
 		Me.btnLimpiar.Text = "    LIMPIAR"
 		Me.btnLimpiar.UseVisualStyleBackColor = false
@@ -215,7 +208,7 @@ Partial Class FrmProveedores
 		Me.btnRegistrar.ForeColor = System.Drawing.Color.White
 		Me.btnRegistrar.Image = CType(resources.GetObject("btnRegistrar.Image"),System.Drawing.Image)
 		Me.btnRegistrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-		Me.btnRegistrar.Location = New System.Drawing.Point(671, 248)
+		Me.btnRegistrar.Location = New System.Drawing.Point(662, 65)
 		Me.btnRegistrar.Name = "btnRegistrar"
 		Me.btnRegistrar.Size = New System.Drawing.Size(191, 57)
 		Me.btnRegistrar.TabIndex = 104
@@ -223,57 +216,75 @@ Partial Class FrmProveedores
 		Me.btnRegistrar.UseVisualStyleBackColor = false
 		AddHandler Me.btnRegistrar.Click, AddressOf Me.BtnRegistrarClick
 		'
-		'txtBRFC
+		'txtBuscarRFC
 		'
-		Me.txtBRFC.BackColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(104,Byte),Integer), CType(CType(130,Byte),Integer))
-		Me.txtBRFC.Font = New System.Drawing.Font("Agency FB", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
-		Me.txtBRFC.ForeColor = System.Drawing.Color.White
-		Me.txtBRFC.Location = New System.Drawing.Point(129, 211)
-		Me.txtBRFC.Name = "txtBRFC"
-		Me.txtBRFC.Size = New System.Drawing.Size(177, 30)
-		Me.txtBRFC.TabIndex = 113
-		Me.txtBRFC.Text = "RFC"
-		Me.txtBRFC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+		Me.txtBuscarRFC.BackColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(104,Byte),Integer), CType(CType(130,Byte),Integer))
+		Me.txtBuscarRFC.Font = New System.Drawing.Font("Agency FB", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic),System.Drawing.FontStyle))
+		Me.txtBuscarRFC.ForeColor = System.Drawing.Color.White
+		Me.txtBuscarRFC.Location = New System.Drawing.Point(129, 211)
+		Me.txtBuscarRFC.Name = "txtBuscarRFC"
+		Me.txtBuscarRFC.Size = New System.Drawing.Size(177, 33)
+		Me.txtBuscarRFC.TabIndex = 113
+		Me.txtBuscarRFC.Text = "Buscar por RFC"
+		AddHandler Me.txtBuscarRFC.TextChanged, AddressOf Me.TxtBuscarRFCTextChanged
 		'
 		'cmbCalifAsc
 		'
 		Me.cmbCalifAsc.BackColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(104,Byte),Integer), CType(CType(130,Byte),Integer))
-		Me.cmbCalifAsc.Font = New System.Drawing.Font("Agency FB", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.cmbCalifAsc.Font = New System.Drawing.Font("Agency FB", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic),System.Drawing.FontStyle))
 		Me.cmbCalifAsc.ForeColor = System.Drawing.Color.White
 		Me.cmbCalifAsc.FormattingEnabled = true
 		Me.cmbCalifAsc.Items.AddRange(New Object() {"Calificación Ascendente", "Calificación Descendente"})
 		Me.cmbCalifAsc.Location = New System.Drawing.Point(465, 211)
 		Me.cmbCalifAsc.Name = "cmbCalifAsc"
-		Me.cmbCalifAsc.Size = New System.Drawing.Size(175, 32)
+		Me.cmbCalifAsc.Size = New System.Drawing.Size(175, 33)
 		Me.cmbCalifAsc.TabIndex = 112
 		Me.cmbCalifAsc.Text = "Calificación Ascendente"
 		'
-		'txtNombreProv
+		'txtBuscarNombreProv
 		'
-		Me.txtNombreProv.BackColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(104,Byte),Integer), CType(CType(130,Byte),Integer))
-		Me.txtNombreProv.Font = New System.Drawing.Font("Agency FB", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
-		Me.txtNombreProv.ForeColor = System.Drawing.Color.White
-		Me.txtNombreProv.Location = New System.Drawing.Point(312, 211)
-		Me.txtNombreProv.Name = "txtNombreProv"
-		Me.txtNombreProv.Size = New System.Drawing.Size(147, 30)
-		Me.txtNombreProv.TabIndex = 111
-		Me.txtNombreProv.Text = "Nombre"
-		Me.txtNombreProv.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
-		AddHandler Me.txtNombreProv.TextChanged, AddressOf Me.TextBoxFiltroNombreProveedorTextChanged
+		Me.txtBuscarNombreProv.BackColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(104,Byte),Integer), CType(CType(130,Byte),Integer))
+		Me.txtBuscarNombreProv.Font = New System.Drawing.Font("Agency FB", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic),System.Drawing.FontStyle))
+		Me.txtBuscarNombreProv.ForeColor = System.Drawing.Color.White
+		Me.txtBuscarNombreProv.Location = New System.Drawing.Point(312, 211)
+		Me.txtBuscarNombreProv.Name = "txtBuscarNombreProv"
+		Me.txtBuscarNombreProv.Size = New System.Drawing.Size(147, 33)
+		Me.txtBuscarNombreProv.TabIndex = 111
+		Me.txtBuscarNombreProv.Text = "Buscar por nombre"
+		AddHandler Me.txtBuscarNombreProv.TextChanged, AddressOf Me.TextBoxFiltroNombreProveedorTextChanged
+		'
+		'dGVProveedores
+		'
+		Me.dGVProveedores.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells
+		Me.dGVProveedores.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells
+		Me.dGVProveedores.BackgroundColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(104,Byte),Integer), CType(CType(130,Byte),Integer))
+		Me.dGVProveedores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+		dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+		dataGridViewCellStyle2.BackColor = System.Drawing.Color.Silver
+		dataGridViewCellStyle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText
+		dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.White
+		dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black
+		dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+		Me.dGVProveedores.DefaultCellStyle = dataGridViewCellStyle2
+		Me.dGVProveedores.Location = New System.Drawing.Point(25, 273)
+		Me.dGVProveedores.Name = "dGVProveedores"
+		Me.dGVProveedores.Size = New System.Drawing.Size(615, 180)
+		Me.dGVProveedores.TabIndex = 114
 		'
 		'FrmProveedores
 		'
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
 		Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(104,Byte),Integer), CType(CType(130,Byte),Integer))
 		Me.ClientSize = New System.Drawing.Size(886, 465)
-		Me.Controls.Add(Me.txtBRFC)
+		Me.Controls.Add(Me.dGVProveedores)
+		Me.Controls.Add(Me.txtBuscarRFC)
 		Me.Controls.Add(Me.cmbCalifAsc)
-		Me.Controls.Add(Me.txtNombreProv)
+		Me.Controls.Add(Me.txtBuscarNombreProv)
 		Me.Controls.Add(Me.btnBuscar)
 		Me.Controls.Add(Me.lbltBuscar)
 		Me.Controls.Add(Me.btnActualizar)
 		Me.Controls.Add(Me.btnEliminar)
-		Me.Controls.Add(Me.dGVProveedores)
 		Me.Controls.Add(Me.btnLimpiar)
 		Me.Controls.Add(Me.btnRegistrar)
 		Me.Controls.Add(Me.lblCalificacion)
@@ -291,12 +302,12 @@ Partial Class FrmProveedores
 		Me.ResumeLayout(false)
 		Me.PerformLayout
 	End Sub
-	Private txtNombreProv As System.Windows.Forms.TextBox
+	Private dGVProveedores As System.Windows.Forms.DataGridView
+	Private txtBuscarNombreProv As System.Windows.Forms.TextBox
 	Private cmbCalifAsc As System.Windows.Forms.ComboBox
-	Private txtBRFC As System.Windows.Forms.TextBox
+	Private txtBuscarRFC As System.Windows.Forms.TextBox
 	Private btnRegistrar As System.Windows.Forms.Button
 	Private btnLimpiar As System.Windows.Forms.Button
-	Private dGVProveedores As System.Windows.Forms.DataGridView
 	Private btnEliminar As System.Windows.Forms.Button
 	Private btnActualizar As System.Windows.Forms.Button
 	Private lbltBuscar As System.Windows.Forms.Label
