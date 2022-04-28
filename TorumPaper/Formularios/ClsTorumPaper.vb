1c9953e54c8b6cfa341bbdc99ca4909963410023
Code <codededemonscm@gmail.com>
2022-04-27 22:50:29 -0500

    Agregando a la clase

--- TorumPaper/Formularios/ClsTorumPaper.vb
+++ TorumPaper/Formularios/ClsTorumPaper.vb
@@ -30,5 +30,33 @@ Public Class ClsTorumPaper
 		End Try
 		Return False
 	End Function
+
+	
+	Public Function traerDatos (ConsultaString As String) As Data.DataTable
+		
+		Dim t As New Data.DataTable
+		Dim cadCon As String="server=localhost;user id=root; password=; database=torumpaper; pooling=false"
+		Dim conn As New MySql.Data.MySqlClient.MySqlConnection(cadCon)
+		Dim da As New MySql.Data.MySqlClient.MySqlDataAdapter(ConsultaString, conn)
+		conn.Open
+		da.Fill(t)
+		conn.Close
+		
+		Return t
+	End Function
+	
+	
+	
+	Public Function consultasSql(consulta As String) As Data.DataTable
+		
+		Dim tablaQuery As New Data.DataTable
+		Dim sqlQuery As MySqlDataAdapter
+		sqlQuery = New MySqlDataAdapter(consulta, MysqlConector)
+		MysqlConector.Open
+		sqlQuery.Fill(tablaQuery)
+		MysqlConector.Close
+		Return tablaQuery
+	End Function
+	
 	
 End Class
